47290530bf11ffb9ba194fc2560b7dd35c7b8fbe
sunil0039git <sunil0039@gmail.com>
2025-07-05 13:21:46 +0530

    1commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "4.32.0"
+        }
+    }
+    
+    backend "azurerm" {
+        resource_group_name  = "rg01s"
+        storage_account_name = "sa01s"
+        container_name       = "tfstate"
+        key                 = "terraform.tfstate"
+      
+    }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "ae3dc4b8-c6c2-443e-b765-214338d62948"
+  
+}
+
+
+
+
